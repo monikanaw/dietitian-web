4c4d4380e3dfa83fa4b70ed3419d15e01f5ed297
Monika Nawrocka <monikanawrocka@onet.pl>
2019-07-16 17:20:49 +0200

    prevent scrolling map

--- mainjs.js
+++ mainjs.js
@@ -1,26 +1,31 @@
-$(document).ready(function() {
-  $(function () {
-    $(document).scroll(function () {
-      var $nav = $(".header-nav");
-      var $drop = $(".drop-down-menu")
-      $nav.toggleClass('header-nav-2', $(this).scrollTop() > $nav.height());
-      $drop.toggleClass('drop-down-menu-2', $(this).scrollTop() > $nav.height());
-    });
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".header-nav");
+    var $drop = $(".drop-down-menu")
+    $nav.toggleClass('header-nav-2', $(this).scrollTop() > $nav.height());
+    $drop.toggleClass('drop-down-menu-2', $(this).scrollTop() > $nav.height());
   });
+});
 
-    map = new OpenLayers.Map("mapdiv");
-    map.addLayer(new OpenLayers.Layer.OSM());
+$(document).ready(function() {
+  if(document.title == "Harmonydiet") {
+    $('#dropDownMenu').addClass("drop-down-menu")
+  } else {
+    $('#dropDownMenu').addClass("drop-down-menu-2")
+  }
 
-    var lonLat = new OpenLayers.LonLat(  17.0347980 , 51.1110390 )
-          .transform(
-            new OpenLayers.Projection("EPSG:4326"),
-            map.getProjectionObject()
-          );
-    var zoom=18;
+   var map = L.map('map').setView([51.1110390,17.0347980], 18);
 
-    var markers = new OpenLayers.Layer.Markers( "Markers" );
-    map.addLayer(markers);
+   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+   }).addTo(map);
 
-    markers.addMarker(new OpenLayers.Marker(lonLat));
-    map.setCenter (lonLat, zoom);
+   var marker = L.marker([51.1110390,17.0347980]).addTo(map);
+
+   var popup = marker.bindPopup('Harmonydiet');
+   popup.openPopup();
+
+   map.scrollWheelZoom.disable();
+    this.map.on('click', () => { this.map.scrollWheelZoom.enable();});
+    this.map.on('mouseout', () => { this.map.scrollWheelZoom.disable();});
 });
